53c1df96519fbcb1181ab69ee981501a0a2788fb
Manoj Kumar Pienti <manojkumar.pienti@cynosure.com>
2023-12-05 13:32:03 +0530

    changed the config file

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,20 @@
-yum install nginx -y 
-rm -rf /usr/share/nginx/html/* 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
-cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
-systemctl enable nginx 
-systemctl start nginx 
+#!/bin/bash
+
+# Install Nginx from Amazon Linux Extras
+sudo amazon-linux-extras install nginx1 -y
+
+# Create the directory if it doesn't exist
+sudo mkdir -p /usr/share/nginx/html
+
+# Download the frontend zip file
+sudo curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+# Navigate to the Nginx HTML directory
+cd /usr/share/nginx/html
+
+# Unzip the frontend zip file without prompting
+sudo unzip -o /tmp/frontend.zip
+
+# Enable and start the Nginx service
+sudo systemctl enable nginx
+sudo systemctl start nginx
